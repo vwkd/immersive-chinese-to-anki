d694e1192ff8bce461c6d63eddbeb5ef0aaf960b
vwkd <33468089+vwkd@users.noreply.github.com>
2021-10-20 13:59:02 +0300

    add rename

---
+++ rename.js
@@ -0,0 +1,41 @@
+import { walk } from "https://deno.land/std@0.112.0/fs/mod.ts";
+import { parse as parsePath, join } from "https://deno.land/std@0.112.0/path/mod.ts";
+import { log } from "./logger.js";
+
+const TARGET_AUDIO_DIR = "audios";
+const DRY_RUN = true;
+
+await renameAudios();
+
+async function renameAudios() {
+  log.info("Renaming audios...")
+
+  const promises = [];
+
+  for await (const entry of walk(TARGET_AUDIO_DIR, { includeDirs: false, exts: [".mp4"] })) {
+    const path = entry.path;
+
+    const { root, dir, base, name, ext } = parsePath(path);
+    let newName = "IC ";
+
+    const regex = /^(.+) (Slow)(.+)$/;
+    const matches = name.match(regex);
+
+    if (matches) {
+      // slow audio
+      newName += matches[1] + matches[3] + " " + matches[2];
+    } else {
+      newName += name;
+    }
+
+    const newBase = newName + ext;
+    const newPath = join(root, dir, newBase);
+
+    console.log(base, "->", newBase);
+    if (!DRY_RUN) {
+      promises.push(Deno.rename(path, newPath));
+    }
+  }
+
+  await Promise.all(promises);
+}
