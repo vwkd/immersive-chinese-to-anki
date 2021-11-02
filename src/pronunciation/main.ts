bfe3b8135938493c5ba514225bbf69aed53e647b
vwkd <33468089+vwkd@users.noreply.github.com>
2021-11-02 20:17:12 +0200

    add pronunciation

---
+++ src/pronunciation/main.ts
@@ -0,0 +1,205 @@
+/**
+ * Create Anki CSV and download audio for IC Pronunciation
+ * Assumes slow audio is duplicate of fast, discards it
+ */
+
+import { log } from "../logger.ts";
+import { BufReader } from "https://deno.land/std@0.112.0/io/mod.ts";
+import {
+  parse as parseCsv,
+  stringify as stringifyCsv,
+} from "https://deno.land/std@0.112.0/encoding/csv.ts";
+import {
+  join,
+  parse as parsePath,
+} from "https://deno.land/std@0.112.0/path/mod.ts";
+import { delay, exists, fetchFile, writeFile } from "../utilities.ts";
+
+const DOWNLOAD_DELAY = 1000;
+const COLUMNS_INPUT = [
+  "lesson",
+  "pinyin",
+  "description",
+  "audioFastUrl",
+  "audioSlowUrl",
+  "identifier",
+];
+const COLUMNS_OUTPUT = [
+  "identifier",
+  "pinyin",
+  "description",
+  "audio",
+];
+
+const [SOURCE_CSV, TARGET_CSV_DIR, TARGET_AUDIO_DIR] = Deno.args;
+
+if (!SOURCE_CSV) {
+  throw new Error("No source file specified");
+}
+if (!TARGET_CSV_DIR) {
+  throw new Error("No target directory specified");
+}
+
+const parsed = await loadPronunciations(SOURCE_CSV);
+const pronunciations = processPronunciations(parsed);
+await writePronunciations(pronunciations);
+if (TARGET_AUDIO_DIR) {
+  await downloadAudios(parsed);
+}
+
+/**
+ * Load pronunciations from CSV
+ */
+async function loadPronunciations(path) {
+  log.info(`Loading pronunciations from '${path}'...`);
+  const file = await Deno.open(path);
+  const input = new BufReader(file);
+  const content = await parseCsv(input, {
+    skipFirstRow: true,
+    /* Throws `Error number of fields line:1`, maybe because other headers have hyphens? */
+    /* columns: COLUMNS_INPUT,*/
+  });
+  Deno.close(file.rid);
+  return content;
+}
+
+/**
+ * Process pronunciations
+ * Returns object with pronunciations as values, pronunciation is object with name and array of exercises
+ * Note, discard slow audio since duplicate of fast
+ */
+function processPronunciations(parsed) {
+  log.info(`Processing pronunciations...`);
+
+  const pronunciations = {};
+
+  parsed.forEach(
+    (
+      {
+        identifier: id,
+        lesson,
+        pinyin,
+        description,
+        audioFastUrl,
+        audioSlowUrl,
+      },
+    ) => {
+      // get name without pinyin words
+      const r1 = /^Pronunciation (.+)\n/;
+      const name = lesson.trim().match(r1)[1];
+
+      const r2 = /\d+/;
+      const lessonNo = name.match(r2)[0];
+      const r3 = /^main_slide-(\d+)$/;
+      const exerciseNo = id.trim().match(r3)[1];
+      const identifier = `P${lessonNo}-${exerciseNo}`;
+
+      log.info(`Processing exercise ${identifier}`);
+
+      const audio = getAudioFileName(audioFastUrl);
+
+      if (audioSlowUrl != audioFastUrl) {
+        throw new Error(
+          `Slow audio is not duplicate of fast audio`,
+        );
+      }
+
+      if (description == "null") {
+        description = "";
+      }
+
+      const exercise = {
+        identifier,
+        pinyin: pinyin.trim(),
+        description: description.trim(),
+        audio,
+      };
+
+      // create pronunciation on first encounter
+      if (pronunciations[name] === undefined) {
+        pronunciations[name] = {};
+        pronunciations[name].exercises = [];
+      }
+      pronunciations[name].name = name;
+      pronunciations[name].exercises.push(exercise);
+    },
+  );
+
+  return pronunciations;
+}
+
+/**
+ * Write processed pronunciations to CSV
+ * Skips files that already exist
+ * Note, doesn't use header since Anki can't skip it
+ */
+async function writePronunciations(pronunciations) {
+  log.info(`Writing pronunciations to '${TARGET_CSV_DIR}'...`);
+
+  const promises = [];
+
+  for (const { name, exercises } of Object.values(pronunciations)) {
+    log.info(`Writing pronunciation ${name}...`);
+    const pronunciationPath = join(TARGET_CSV_DIR, name + ".csv");
+
+    const csvString = await stringifyCsv(exercises, COLUMNS_OUTPUT, {
+      headers: false,
+    });
+    if (await exists(pronunciationPath)) {
+      log.debug(
+        `Skip writing pronunciation because already exists.`,
+      );
+    } else {
+      log.debug(`Write pronunciation ${name}.`);
+      promises.push(Deno.writeTextFile(pronunciationPath, csvString));
+    }
+  }
+
+  await Promise.all(promises);
+}
+
+/**
+ * Download audio files
+ * Skips files that already exist
+ */
+async function downloadAudios(parsed) {
+  log.info(
+    `Downloading audios into ${TARGET_AUDIO_DIR}... with ${
+      DOWNLOAD_DELAY / 1000
+    } seconds delay`,
+  );
+
+  for (const { audioFastUrl: audioUrl } of parsed) {
+    log.info(`Downloading audio ${getAudioFileName(audioUrl)}...`);
+    let timeout;
+
+    const audioPath = join(
+      TARGET_AUDIO_DIR,
+      getAudioFileName(audioUrl),
+    );
+
+    if (await exists(audioPath)) {
+      log.debug(
+        `Skip downloading audio because already exists.`,
+      );
+    } else {
+      log.debug(`Downloading audio.`);
+      timeout = delay(DOWNLOAD_DELAY);
+      const blob = await fetchFile(audioUrl);
+      await writeFile(audioPath, blob);
+    }
+
+    await timeout;
+  }
+}
+
+/**
+ * Get filename for audio from URL
+ * Add "IC " to beginning
+ */
+function getAudioFileName(url) {
+  const { base } = parsePath(url);
+  const newBase = "IC " + base;
+  log.debug(base, "->", newBase);
+  return newBase;
+}
