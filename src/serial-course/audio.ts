665b96833bde702fe6ca3c45637ddfde4e8ff14c
vwkd <33468089+vwkd@users.noreply.github.com>
2025-06-27 18:24:39 +0200

    src/serial-course: split into separate files

---
+++ src/serial-course/audio.ts
@@ -0,0 +1,60 @@
+import { log } from "../logger.ts";
+import { join } from "@std/path/join";
+import { exists } from "@std/fs/exists";
+import { delay } from "@std/async/delay";
+import { downloadFile } from "../utilities.ts";
+import { getFastAudioFileName, getSlowAudioFileName } from "./utils.ts";
+import type { Data } from "./types.ts";
+
+const DOWNLOAD_DELAY = 1000;
+
+/**
+ * Download audio files
+ * Skips files that already exist
+ */
+export async function downloadAudios(parsed: Data, dir: string): Promise<void> {
+  log.info(
+    `Downloading audios into ${dir}... with ${
+      DOWNLOAD_DELAY / 1000
+    } seconds delay`,
+  );
+
+  for (const { audioFastUrl, audioSlowUrl, identifier } of parsed) {
+    log.info(`Downloading audio for ${identifier}...`);
+    let timeout: Promise<void> | undefined;
+
+    const fastAudioPath = join(
+      dir,
+      getFastAudioFileName(audioFastUrl),
+    );
+
+    if (await exists(fastAudioPath)) {
+      log.debug(
+        `Skip downloading fast audio because already exists.`,
+      );
+    } else {
+      log.debug(`Downloading fast audio.`);
+      timeout = delay(DOWNLOAD_DELAY);
+      await downloadFile(audioFastUrl, fastAudioPath);
+    }
+
+    if (audioSlowUrl != audioFastUrl) {
+      const slowAudioPath = join(
+        dir,
+        getSlowAudioFileName(audioSlowUrl),
+      );
+
+      if (await exists(slowAudioPath)) {
+        log.debug(
+          `Skip downloading slow audio because already exists.`,
+        );
+      } else {
+        log.debug(`Downloading slow audio.`);
+        if (!timeout) timeout = delay(DOWNLOAD_DELAY);
+        await downloadFile(audioSlowUrl, slowAudioPath);
+      }
+    }
+
+    await timeout;
+  }
+}
---
+++ src/serial-course/utils.ts
@@ -0,0 +1,27 @@
+import { log } from "../logger.ts";
+import { parse as parsePath } from "@std/path";
+
+/**
+ * Get filename for fast audio from URL
+ * Add "IC " to beginning
+ */
+export function getFastAudioFileName(url: string): string {
+  const { base } = parsePath(url);
+  const newBase = "IC " + base;
+  log.debug(base, "->", newBase);
+  return newBase;
+}
+
+/**
+ * Get filename for slow audio from URL
+ * Add "IC " to beginning, move "Slow" to end
+ */
+export function getSlowAudioFileName(url: string): string {
+  const { base, name, ext } = parsePath(url);
+  const regex = /^(.+) (Slow)(.+)$/;
+  const matches = name.match(regex);
+  const newName = "IC " + matches[1] + matches[3] + " " + matches[2];
+  const newBase = newName + ext;
+  log.debug(base, "->", newBase);
+  return newBase;
+}
